10d3f586598c859e6c2c9076ddc6a0032408fd52
Michael Mokry <mm117s@att.com>
2017-08-31 01:29:25 -0500

    Adding Junits for ONAP-PAP-REST
    Change-Id: Ibcd972e9fdf8ebd5f17ad7140f959eda5456d040
    Issue-ID: POLICY-32
    Signed-off-by: Michael Mokry <mm117s@att.com>

---
+++ ONAP-PAP-REST/src/test/java/org/onap/policy/pap/xacml/rest/components/ActionPolicyTest.java
@@ -0,0 +1,147 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP-PAP-REST
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.pap.xacml.rest.components;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.onap.policy.common.logging.flexlogger.FlexLogger;
+import org.onap.policy.common.logging.flexlogger.Logger;
+import org.onap.policy.rest.adapter.PolicyRestAdapter;
+
+
+public class ActionPolicyTest {
+	
+	private static Logger logger = FlexLogger.getLogger(ActionPolicyTest.class);
+	PolicyRestAdapter policyAdapter = new PolicyRestAdapter();
+	List<String> dynamicRuleAlgorithmLabels = new LinkedList<>();
+	List<String> dynamicRuleAlgorithmCombo = new LinkedList<>();
+	List<String> dynamicRuleAlgorithmField1 = new LinkedList<>();
+	List<String> dynamicRuleAlgorithmField2 = new LinkedList<>();
+	Map<String, String> attributeMap = new HashMap<>();
+	ActionPolicy component = null;
+
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		logger.info("setUp: Entering");
+
+		dynamicRuleAlgorithmLabels.add("test");
+		dynamicRuleAlgorithmField1.add("testField1");
+		dynamicRuleAlgorithmCombo.add("testCombo");
+		dynamicRuleAlgorithmField2.add("testField2");
+		
+		policyAdapter.setPolicyName("Mike.Action_junitTest.1.xml");
+		policyAdapter.setPolicyDescription("test");
+		policyAdapter.setRuleCombiningAlgId("urn:oasis:names:tc:xacml:3.0:rule-combining-algorithm:permit-overrides");
+		policyAdapter.setPolicyType("Action");
+		policyAdapter.setEditPolicy(false);
+		policyAdapter.setDomainDir("src/test/resources/client.properties");
+		policyAdapter.setDomain("Test");
+		policyAdapter.setNewFileName("/src/test/resources/Test/client.properties");
+		policyAdapter.setHighestVersion(1);
+		policyAdapter.setPolicyID("urn:xacml:policy:id:"+UUID.randomUUID());
+		
+		policyAdapter.setActionDictHeader("");
+		policyAdapter.setActionDictType("REST");
+		policyAdapter.setActionDictUrl("onap.org");
+		policyAdapter.setActionDictMethod("GET");
+		policyAdapter.setActionAttribute("test");
+		policyAdapter.setActionBody("test");
+		
+		policyAdapter.setDynamicRuleAlgorithmLabels(dynamicRuleAlgorithmLabels);
+		policyAdapter.setDynamicRuleAlgorithmCombo(dynamicRuleAlgorithmCombo);
+		policyAdapter.setDynamicRuleAlgorithmField1(dynamicRuleAlgorithmField1);
+		policyAdapter.setDynamicRuleAlgorithmField2(dynamicRuleAlgorithmField2);
+		
+		attributeMap.put("java", "test");
+		policyAdapter.setDynamicFieldConfigAttributes(attributeMap);
+
+		component = new ActionPolicy(policyAdapter);
+
+        logger.info("setUp: exit");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.openecomp.policy.pap.xacml.rest.components.ActionPolicy#savePolicies()}.
+	 */
+	@Test
+	public void testSavePolicies() {
+        Policy mockPolicy = Mockito.mock(Policy.class);
+        ActionPolicy mockAction = Mockito.mock(component.getClass());
+		
+        Map<String, String> successMap = new HashMap<>();
+        Map<String, String> oldsuccessMap = new HashMap<>();
+        
+		successMap.put("success", "success");
+        when(mockPolicy.createPolicy(Paths.get(policyAdapter.getNewFileName()), policyAdapter.getData())).thenReturn(successMap);
+
+        try {
+        	when(mockAction.savePolicies()).thenReturn(successMap);
+        	successMap = mockAction.savePolicies();
+			oldsuccessMap = component.savePolicies();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(successMap.get("success"),"success");
+		assertNull(oldsuccessMap);
+		
+	}
+
+	/**
+	 * Test method for {@link org.openecomp.policy.pap.xacml.rest.components.ActionPolicy#prepareToSave()}.
+	 */
+	@Test
+	public void testPrepareToSave() {
+		logger.debug("test PrepareToSave Policy: enter");
+		boolean response = false;
+		
+		try {
+			response = component.prepareToSave();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertTrue(response);	
+		
+	}
+
+}
---
+++ ONAP-PAP-REST/src/test/java/org/onap/policy/pap/xacml/rest/components/FirewallConfigPolicyTest.java
@@ -0,0 +1,138 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP-PAP-REST
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.pap.xacml.rest.components;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.onap.policy.common.logging.flexlogger.FlexLogger;
+import org.onap.policy.common.logging.flexlogger.Logger;
+import org.onap.policy.pap.xacml.rest.XACMLPapServlet;
+import org.onap.policy.rest.adapter.PolicyRestAdapter;
+
+import com.att.research.xacml.util.XACMLProperties;
+
+public class FirewallConfigPolicyTest {
+	
+	private static Logger logger = FlexLogger.getLogger(FirewallConfigPolicyTest.class);
+	PolicyRestAdapter policyAdapter = new PolicyRestAdapter();
+	Map<String, String> attributeMap = new HashMap<>();
+	FirewallConfigPolicy component = null;
+    FirewallConfigPolicy mockFWConfig = null;
+	private static EntityManagerFactory emf;
+
+
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		logger.info("setup: enter");
+		
+		policyAdapter.setPolicyName("FWjunitTest");
+		policyAdapter.setPolicyDescription("test");
+		policyAdapter.setRuleCombiningAlgId("urn:oasis:names:tc:xacml:3.0:rule-combining-algorithm:permit-overrides");
+		policyAdapter.setPolicyType("Config");
+		policyAdapter.setConfigPolicyType("Firewall Config");
+		policyAdapter.setEditPolicy(false);
+		policyAdapter.setDomainDir("src/test/resources/client.properties");
+		policyAdapter.setDomain("Test");
+		policyAdapter.setNewFileName("Test.Config_FW_junitTest.1.xml");
+		policyAdapter.setHighestVersion(1);
+		policyAdapter.setVersion(String.valueOf(1));
+		policyAdapter.setPolicyID("urn:xacml:policy:id:"+UUID.randomUUID());
+		policyAdapter.setRuleID("");
+		policyAdapter.setConfigName("testname");
+		policyAdapter.setGuard("True");
+		policyAdapter.setRiskLevel("3");
+		policyAdapter.setRiskType("RiskTest");
+		policyAdapter.setSecurityZone("CraigSecurityZone");
+		policyAdapter.setUserId("API");
+		
+		attributeMap.put("testJunits", "test");
+		policyAdapter.setDynamicFieldConfigAttributes(attributeMap);
+		Properties properties;
+		properties = XACMLProperties.getProperties();
+		emf = Persistence.createEntityManagerFactory("testPapPU");
+		component = new FirewallConfigPolicy(policyAdapter);
+		mockFWConfig = Mockito.mock(FirewallConfigPolicy.class);
+        logger.info("setUp: exit");
+		
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.openecomp.policy.pap.xacml.rest.components.FirewallConfigPolicy#savePolicies()}.
+	 */
+	@Test
+	public void testSavePolicies() {
+        Map<String, String> successMap = new HashMap<>();
+        successMap.put("success", "success");
+        when(mockFWConfig.createPolicy(Paths.get(policyAdapter.getNewFileName()), policyAdapter.getData())).thenReturn(successMap);
+		try {
+	        when(mockFWConfig.savePolicies()).thenReturn(successMap);
+	        successMap = mockFWConfig.savePolicies();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test method for {@link org.openecomp.policy.pap.xacml.rest.components.FirewallConfigPolicy#prepareToSave()}.
+	 */
+	@Test
+	public void testPrepareToSave() {
+		logger.debug("test prepareToSave Policy: enter");
+		EntityManager em = emf.createEntityManager();
+		boolean response = false;
+		try {
+	        when(mockFWConfig.prepareToSave()).thenReturn(true);
+			response = mockFWConfig.prepareToSave();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertTrue(response);		
+		
+	}
+
+}
