6e2d9d53bedf7b1239eb57bd58102abc3bed98db
uj426b <jauhari.utkarsh@gmail.com>
2018-02-06 11:13:21 -0500

    Adding JUNITs for ONAP-PAP-REST
    Issue-ID: POLICY-338
    Change-Id: I123a92872a236b23144c59e87ea2dffc9ee7fc48
    Signed-off-by: uj426b <uj426b@att.com>

--- ONAP-PAP-REST/src/main/java/org/onap/policy/pap/xacml/rest/controller/BRMSDictionaryController.java
+++ ONAP-PAP-REST/src/main/java/org/onap/policy/pap/xacml/rest/controller/BRMSDictionaryController.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP-PAP-REST
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -85,6 +85,9 @@ public class BRMSDictionaryController{
 	@Autowired
 	public BRMSDictionaryController(CommonClassDao commonClassDao){
 		BRMSDictionaryController.commonClassDao = commonClassDao;
+	}
+	public static void setCommonClassDao(CommonClassDao commonClassDao2) {
+		BRMSDictionaryController.commonClassDao = commonClassDao2;
 	}
 	/*
 	 * This is an empty constructor
