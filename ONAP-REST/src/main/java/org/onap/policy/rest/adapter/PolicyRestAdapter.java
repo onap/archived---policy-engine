76edc74692e71f9a343e6d1d0316df37346c9a25
rb7147 <rb7147@att.com>
2018-02-09 14:30:14 -0500

    Added Junits for ONAP-PAP-REST
    Cleaned up unused imports.
    Cleaned up unused code.
    Added Junits for Dictionary controllers.
    
    Issue-ID: POLICY-338
    Change-Id: If29b934e95fdaf60dcf82300a95a23ac3c980419
    Signed-off-by: rb7147 <rb7147@att.com>

--- ONAP-REST/src/main/java/org/onap/policy/rest/adapter/PolicyRestAdapter.java
+++ ONAP-REST/src/main/java/org/onap/policy/rest/adapter/PolicyRestAdapter.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP-PAP-REST
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@
 
 package org.onap.policy.rest.adapter;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
