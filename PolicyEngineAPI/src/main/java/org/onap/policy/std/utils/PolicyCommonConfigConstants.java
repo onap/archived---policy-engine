e0047af3eadd9e93c863e15eb1a7b5448875bab7
waqas.ikram <waqas.ikram@ericsson.com>
2018-02-23 11:17:30 +0000

    Add JUnit tests policy/engine PolicyEngineAPI
    Change-Id: I8d7a76836759b042c83349c2b875fb77085b531c
    Issue-ID: POLICY-606
    Signed-off-by: waqas.ikram <waqas.ikram@ericsson.com>

---
+++ PolicyEngineAPI/src/main/java/org/onap/policy/std/utils/PolicyCommonConfigConstants.java
@@ -0,0 +1,30 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * PolicyCommonConfigConstants
+ * ================================================================================
+ * Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.std.utils;
+
+public final class PolicyCommonConfigConstants {
+
+    public static final String CONFIG_NAME = "ConfigName";
+
+    public static final String ONAP_NAME = "ONAPName";
+
+    private PolicyCommonConfigConstants() {}
+}
