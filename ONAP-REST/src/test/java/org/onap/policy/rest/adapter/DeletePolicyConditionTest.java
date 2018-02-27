b5ac91f811ecd9a1c22f33441aa0fd5a6dafcdbf
bobbymander <bobby.mander@att.com>
2018-02-27 13:51:51 -0500

    JUnit additions for XACML,REST,POLICY-SDK
    Issue-ID: POLICY-584
    Change-Id: I4bb91b18122c7f61bb71b1a9e39d51cd65af70bd
    Signed-off-by: bobbymander <bobby.mander@att.com>

---
+++ ONAP-REST/src/test/java/org/onap/policy/rest/adapter/DeletePolicyConditionTest.java
@@ -0,0 +1,32 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Engine
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.rest.adapter;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class DeletePolicyConditionTest {
+
+	@Test
+	public void testDeletePolicyCondition(){
+		assertEquals(DeletePolicyCondition.ONE.toString(), "Are you sure you want to delete Current Version of policy");
+		assertEquals(DeletePolicyCondition.ALL.toString(), "Are you sure you want to delete All Versions of policy");
+	}
+}
