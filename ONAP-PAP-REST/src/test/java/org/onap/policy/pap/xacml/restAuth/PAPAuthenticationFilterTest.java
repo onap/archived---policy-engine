f0c563df34a999a1702d51cb6cb44fe10155b16b
rb7147 <rb7147@att.com>
2018-02-28 14:23:12 -0500

    Added Junits for Policy PAP-REST
    Issue-ID: POLICY-600
    Change-Id: Icb67ac587e614f663416b3ea0a03ca6e5b02c621
    Signed-off-by: rb7147 <rb7147@att.com>

--- ONAP-PAP-REST/src/test/java/org/onap/policy/pap/xacml/restAuth/PAPAuthenticationFilterTest.java
+++ ONAP-PAP-REST/src/test/java/org/onap/policy/pap/xacml/restAuth/PAPAuthenticationFilterTest.java
@@ -17,19 +17,47 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
-//foo
 package org.onap.policy.pap.xacml.restAuth;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
 import java.io.IOException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
 import com.mockrunner.mock.web.MockHttpServletRequest;
 import com.mockrunner.mock.web.MockHttpServletResponse;
 
 public class PAPAuthenticationFilterTest {
+	
+	private HttpServletRequest request = null;
+	private HttpServletResponse response = null;
+	private String oldProperty;
+	private String systemKey = "xacml.properties";
+	private FilterChain filter;
+	private PAPAuthenticationFilter papFilter;
+	
+	@Before
+	public void setUp(){
+		// Set the system property temporarily
+		oldProperty = System.getProperty(systemKey);
+		System.setProperty(systemKey, "xacml.pap.properties");	
+		
+		request = Mockito.mock(HttpServletRequest.class);
+		response = Mockito.mock(HttpServletResponse.class);
+		filter = Mockito.mock(FilterChain.class);
+		papFilter = new PAPAuthenticationFilter();
+	}
+	
 	@Test
 	public void testAuth() throws IOException, ServletException {
 		PAPAuthenticationFilter filter = new PAPAuthenticationFilter();
@@ -49,6 +77,45 @@ public class PAPAuthenticationFilterTest {
 		}
 		catch (Exception ex) {
 			fail("Not expecting any exceptions.");
+		}
+	}
+	
+	@Test
+	public void testDoFilter() {
+		Mockito.when(request.getRequestURI()).thenReturn("/pap/");
+		Mockito.when(request.getHeader("Authorization")).thenReturn("Basic dGVzdHBhcDphbHBoYTEyMw==");
+		callDoFilter();
+		Mockito.when(request.getRequestURI()).thenReturn("/pap/onap/");
+		callDoFilter();
+	}
+	
+	public void callDoFilter(){
+		try {
+			papFilter.doFilter(request, response, filter);
+		} catch (Exception e) {
+			assertEquals(NullPointerException.class, e.getClass());
+		} 
+	}
+	
+	@Test
+	public void testOnPassingInvalidParamters(){
+		Mockito.when(request.getRequestURI()).thenReturn("/pap/");
+		Mockito.when(request.getHeader("Authorization")).thenReturn("Basic dGVzdHBhcDphbHBoYTE789==");
+		try {
+			papFilter.doFilter(request, response, filter);
+			assertEquals(0, response.getStatus());
+		} catch (Exception e) {
+			fail();
+		}
+	}
+	
+	@After
+	public void reset(){
+		// Restore the original system property
+		if (oldProperty != null) {
+			System.setProperty(systemKey, oldProperty);
+		} else {
+			System.clearProperty(systemKey);
 		}
 	}
 }
