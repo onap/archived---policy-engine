2f5c4c32d059f8cad56ef1655fe97e5a23285c81
bobbymander <bobby.mander@att.com>
2018-03-01 14:59:12 -0500

    JUnit additions for PAP-REST,API
    Issue-ID: POLICY-600
    Change-Id: I68020230d370950c0323a544910ad54015e1a08a
    Signed-off-by: bobbymander <bobby.mander@att.com>

---
+++ ONAP-PAP-REST/src/test/java/org/onap/policy/pap/xacml/rest/components/BRMSPolicyTest.java
@@ -0,0 +1,54 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP-PAP-REST
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.pap.xacml.rest.components;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import java.io.IOException;
+import org.onap.policy.rest.dao.CommonClassDao;
+
+public class BRMSPolicyTest {
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testConstructor1() {
+		CreateBRMSRuleTemplate template = new CreateBRMSRuleTemplate();
+		assertNotNull(template);
+	}
+	
+	@Test
+	public void testConstructor2() {
+		CommonClassDao commonClassDao = null;
+		CreateBRMSRuleTemplate template = new CreateBRMSRuleTemplate(commonClassDao);
+		assertNotNull(template);
+	}
+	
+	@Test
+	public void testReadFile() throws IOException {
+		String goodRule = "declare PapParams\nparam1 : int\nend\n";
+		String badRule = "declare PapParams\nparam1+ : int\nend\n";
+		assertEquals(CreateBRMSRuleTemplate.validateRuleParams(goodRule), true);
+		assertEquals(CreateBRMSRuleTemplate.validateRuleParams(badRule), false);
+	}
+}
---
+++ ONAP-PAP-REST/src/test/java/org/onap/policy/pap/xacml/rest/components/MicroServicePolicyTest.java
@@ -0,0 +1,62 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP-PAP-REST
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.pap.xacml.rest.components;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+import org.onap.policy.rest.adapter.PolicyRestAdapter;
+import com.att.research.xacml.api.pap.PAPException;
+
+public class MicroServicePolicyTest {
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testConstructor1() {
+		thrown.expect(NullPointerException.class);
+		MicroServiceConfigPolicy policy = new MicroServiceConfigPolicy();
+		policy.getCorrectPolicyDataObject();
+		fail("Expected an exception");
+	}
+	
+	@Test
+	public void testConstructor2() {
+		PolicyRestAdapter policyAdapter = new PolicyRestAdapter();
+		MicroServiceConfigPolicy policy = new MicroServiceConfigPolicy(policyAdapter);
+		assertNull(policy.getCorrectPolicyDataObject());
+	}
+	
+	@Test
+	public void testPrepareToSave() throws PAPException {
+		thrown.expect(NullPointerException.class);
+		PolicyRestAdapter policyAdapter = new PolicyRestAdapter();
+		MicroServiceConfigPolicy policy = new MicroServiceConfigPolicy(policyAdapter);
+		policyAdapter.setHighestVersion(1);
+		policyAdapter.setPolicyType("Config");
+		policyAdapter.setNewFileName("foo.xml");
+		policyAdapter.setJsonBody("{ \"version\": \"1.0\"}");
+		policyAdapter.setServiceType("foo");
+		policy.prepareToSave();
+		fail("Expected an exception");
+	}
+}
