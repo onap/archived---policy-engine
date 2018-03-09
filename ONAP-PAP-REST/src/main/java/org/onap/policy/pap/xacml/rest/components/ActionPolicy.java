1a1fb9567c10bd4fc135b0a37ed68c29e7ed4885
Manamohan Satapathy <MS00534989@techmahindra.com>
2018-03-09 13:29:39 +0530

    Policy:Blocker
    Use try-with-resources for closeing the FileWriter:L264 L265
    Issue-ID: POLICY-683
    
    Sonar Link: https://sonar.onap.org/issues?myIssues=true&open=AWF91iwf8TZzbCgU6arv&resolved=false
    
    Location:ONAP-PAP-REST\src\main\java\org\onap\policy\pap\xacml\rest\components\ActionPolicy.java
    
    Change-Id: Ie9adb5b50dbb61c923bf73fa107fd19366f24304
    Signed-off-by: Manamohan Satapathy <MS00534989@techmahindra.com>

--- ONAP-PAP-REST/src/main/java/org/onap/policy/pap/xacml/rest/components/ActionPolicy.java
+++ ONAP-PAP-REST/src/main/java/org/onap/policy/pap/xacml/rest/components/ActionPolicy.java
@@ -256,13 +256,14 @@ public class ActionPolicy extends Policy {
 
     // Saving the json Configurations file if exists at server location for action policy.
     private void saveActionBody(String policyName, String actionBodyData) {
-        try {
+        
             if(policyName.endsWith(".xml")){
                 policyName = policyName.replace(".xml", "");
             }
             File file = new File(ACTION_HOME+ File.separator + policyName + ".json");
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
+            
+           
+			try (FileWriter fw = new FileWriter(file.getAbsoluteFile()); BufferedWriter bw = new BufferedWriter(fw)){
             bw.write(actionBodyData);
             bw.close();
             if (LOGGER.isInfoEnabled()) {
