4ea6678829127d8333e53f6944c54ec475fdcb5b
mmis <michael.morris@ericsson.com>
2018-03-14 12:03:37 +0000

    Renamed policy-pe docker image
    Renamed from:
    onap/policy/policy-pe
    to
    onap/policy-pe
    to enable consistent naming across nexus3 and docker hub
    
    Issue-ID: POLICY-674
    Change-Id: I1ace3e6034a17afa1ed68198b367e17981ec388b
    Signed-off-by: mmis <michael.morris@ericsson.com>

--- docker_build.sh
+++ docker_build.sh
@@ -46,19 +46,19 @@ echo "Building $IMAGE"
 #
 # This is the local latest tagged image. The Dockerfile's need this to build images
 #
-TAGS="--tag onap/policy/${IMAGE}:latest"
+TAGS="--tag onap/${IMAGE}:latest"
 #
 # This is the nexus repo prepended for latest tagged image.
 #
-TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:latest"
+TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/${IMAGE}:latest"
 #
 # This has the nexus repo prepended and only major/minor version with latest
 #
-TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:${MVN_MAJMIN_VERSION}-latest"
+TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/${IMAGE}:${MVN_MAJMIN_VERSION}-latest"
 #
 # This has the nexus repo prepended and major/minor/patch version with timestamp
 #
-TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:${MVN_VERSION}-STAGING-${TIMESTAMP}"
+TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/${IMAGE}:${MVN_VERSION}-STAGING-${TIMESTAMP}"
 
 echo $TAGS
 
@@ -75,7 +75,7 @@ docker images
 
 echo "Pushing $IMAGE"
 
-docker push ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:latest
+docker push ${DOCKER_REPOSITORY}/onap/${IMAGE}:latest
 
 if [ $? -ne 0 ]
 then
@@ -84,7 +84,7 @@ then
 
 fi
 
-docker push ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:${MVN_MAJMIN_VERSION}-latest
+docker push ${DOCKER_REPOSITORY}/onap/${IMAGE}:${MVN_MAJMIN_VERSION}-latest
 
 if [ $? -ne 0 ]
 then
@@ -92,7 +92,7 @@ then
     exit 1
 
 fi
-docker push ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:${MVN_VERSION}-STAGING-${TIMESTAMP}
+docker push ${DOCKER_REPOSITORY}/onap/${IMAGE}:${MVN_VERSION}-STAGING-${TIMESTAMP}
 
 if [ $? -ne 0 ]
 then
--- docker_verify.sh
+++ docker_verify.sh
@@ -49,15 +49,15 @@ echo "Building $IMAGE"
 #
 # This is the local latest tagged image. The Dockerfile's need this to build images
 #
-TAGS="--tag onap/policy/${IMAGE}:latest"
+TAGS="--tag onap/${IMAGE}:latest"
 #
 # This has the nexus repo prepended and only major/minor version with latest
 #
-TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:${MVN_MAJMIN_VERSION}-latest"
+TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/${IMAGE}:${MVN_MAJMIN_VERSION}-latest"
 #
 # This has the nexus repo prepended and major/minor/patch version with timestamp
 #
-TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/policy/${IMAGE}:${MVN_VERSION}-${TIMESTAMP}"
+TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/onap/${IMAGE}:${MVN_VERSION}-${TIMESTAMP}"
 
 echo $TAGS
 
