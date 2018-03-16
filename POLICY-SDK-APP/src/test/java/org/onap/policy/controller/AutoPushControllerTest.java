7ef55cfb9f400e31732942a6b0a723dab48a3cc7
bobbymander <bobby.mander@att.com>
2018-03-16 09:38:23 -0400

    JUnit additions for SDK-APP
    Issue-ID: POLICY-604
    Change-Id: I2e2cdb3037a70af7d8d85a4a6274b3e361510bc1
    Signed-off-by: bobbymander <bobby.mander@att.com>

--- POLICY-SDK-APP/src/test/java/org/onap/policy/controller/AutoPushControllerTest.java
+++ POLICY-SDK-APP/src/test/java/org/onap/policy/controller/AutoPushControllerTest.java
@@ -22,43 +22,86 @@ package org.onap.policy.controller;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.onap.portalsdk.core.domain.User;
+import org.onap.portalsdk.core.web.support.UserUtils;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
 
+@RunWith(PowerMockRunner.class)
 public class AutoPushControllerTest {
-	private PolicyController controller = new PolicyController();;
-	private AutoPushController apController = new AutoPushController();
-	
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
-	
-	@Test
-	public void testAutoPushSetGet() throws IOException {
-		// Get and set tests
-		apController.setPolicyController(controller);
-		assertEquals(apController.getPolicyController(), controller);
-	}
-	
-	@Test
-	public void testNegativeCase1() {
-		try {
-			apController.getPolicyGroupContainerData(null,  null);
-		}
-		catch (Exception ex) {
-			fail("No exceptions expected, received: " + ex);
-		}
-	}
-		
-	@Test
-	public void testNegativeCase2() throws IOException {
-		thrown.expect(NullPointerException.class);
-		apController.pushPolicyToPDPGroup(null, null);
-	}
+  private PolicyController controller = new PolicyController();;
+  private AutoPushController apController = new AutoPushController();
 
-	@Test
-	public void testNegativeCase3() throws IOException {
-		thrown.expect(NullPointerException.class);
-		apController.removePDPGroup(null, null);
-	}
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void testAutoPushSetGet() throws IOException {
+    // Get and set tests
+    apController.setPolicyController(controller);
+    assertEquals(apController.getPolicyController(), controller);
+  }
+
+  @Test
+  public void testNegativeCase1() {
+    try {
+      apController.getPolicyGroupContainerData(null, null);
+    } catch (Exception ex) {
+      fail("No exceptions expected, received: " + ex);
+    }
+  }
+
+  @Test
+  public void testNegativeCase2() throws IOException {
+    thrown.expect(NullPointerException.class);
+    apController.pushPolicyToPDPGroup(null, null);
+  }
+
+  @Test
+  public void testNegativeCase3() throws IOException {
+    thrown.expect(NullPointerException.class);
+    apController.removePDPGroup(null, null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testRefresh() throws IOException {
+    apController.refreshGroups();
+  }
+
+  @PrepareForTest({UserUtils.class})
+  @Test
+  public void testRequests() throws Exception {
+    // Mock user utilities
+    PowerMockito.mockStatic(UserUtils.class);
+    User user = new User();
+    Mockito.when(UserUtils.getUserSession(Mockito.any())).thenReturn(user);
+
+    // Mock policy controller
+    PolicyController pController = Mockito.mock(PolicyController.class);
+    PowerMockito.whenNew(PolicyController.class).withNoArguments().thenReturn(pController);
+    Mockito.when(pController.getRoles(Mockito.any())).thenReturn(null);
+
+    // Test group container
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    MockHttpServletResponse response = new MockHttpServletResponse();
+    apController.getPolicyGroupContainerData(request, response);
+    assertEquals(response.getStatusCode(), HttpServletResponse.SC_OK);
+
+    // Test push
+    apController.pushPolicyToPDPGroup(request, response);
+    assertEquals(response.getStatusCode(), HttpServletResponse.SC_OK);
+
+    // Test remove
+    apController.removePDPGroup(request, response);
+    assertEquals(response.getStatusCode(), HttpServletResponse.SC_OK);
+  }
 }
