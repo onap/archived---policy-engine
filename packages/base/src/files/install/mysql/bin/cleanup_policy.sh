669931e76de18aabb14f9ff873da4783f8c83d7a
Chou, Joseph (JC2555) <jc2555@att.com>
2018-03-22 11:22:35 -0400

    ONAP code change for log files consolidation
    Code change for log files consolidation of PAP, PDP, BRMSGW, Console and db
    script
    
    Issue-ID: POLICY-562
    Change-Id: I416ba7748b841bb2b1a31682e2b1631e279549e8
    Signed-off-by: Chou, Joseph (JC2555) <jc2555@att.com>

--- packages/base/src/files/install/mysql/bin/cleanup_policy.sh
+++ packages/base/src/files/install/mysql/bin/cleanup_policy.sh
@@ -71,8 +71,12 @@ function cleanup_deleted_policy
 }
 
 # MAIN
-LOG=$POLICY_HOME/logs/cleanup_policy_$DATE.log
-ERR=$POLICY_HOME/logs/cleanup_policy_$DATE.err
+if [ -z ${POLICY_LOGS} ]; then
+  POLICY_LOGS=/var/log/onap
+fi
+mkdir -p $POLICY_LOGS/policy/db
+LOG=$POLICY_LOGS/policy/db/cleanup_policy_$DATE.log
+ERR=$POLICY_LOGS/policy/db/cleanup_policy_$DATE.err
 echo "cleanup_policy.sh started ... `date`" | tee -a $LOG
 if [ $# -eq 3 ]; then 
   DB_USER="${1}"
--- packages/base/src/files/install/mysql/bin/db_backup.sh
+++ packages/base/src/files/install/mysql/bin/db_backup.sh
@@ -63,6 +63,12 @@ function backup_database
 
 
 # MAIN
+if [ -z ${POLICY_LOGS} ]; then
+  POLICY_LOGS=/var/log/onap
+fi
+mkdir -p $POLICY_LOGS/policy/db
+LOG=$POLICY_LOGS/policy/db/db_backup_$DATE.log
+ERR=$POLICY_LOGS/policy/db/db_backup_$DATE.err  
 echo "db_backup.sh started ... `date`" | tee -a $LOG
 if [ $# -eq 3 ]; then 
   DB_USER="${1}"
@@ -71,8 +77,6 @@ if [ $# -eq 3 ]; then
   echo "DB_USER: $DB_USER" | tee -a $LOG
     
   DAILY_BACKUP_DIR=$POLICY_HOME/data/mysql/$DATE
-  LOG=$POLICY_HOME/logs/db_backup_$DATE.log
-  ERR=$POLICY_HOME/logs/db_backup_$DATE.err
   create_backup_dir 
 
   backup_database
