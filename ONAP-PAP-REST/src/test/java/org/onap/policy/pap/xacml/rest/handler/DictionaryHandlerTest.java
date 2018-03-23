502fda1d9df7e1bd396fe0739f779ae3cfe1a8e7
bobbymander <bobby.mander@att.com>
2018-03-23 14:31:13 -0400

    JUnit additions for PAP-REST,REST
    Issue-ID: POLICY-603
    Change-Id: Id7d2c1a835d028e88337db67d55f139aa1764826
    Signed-off-by: bobbymander <bobby.mander@att.com>

---
+++ ONAP-PAP-REST/src/test/java/org/onap/policy/pap/xacml/rest/handler/DictionaryHandlerTest.java
@@ -0,0 +1,40 @@
+/*
+ * ============LICENSE_START======================================================= ONAP-PAP-REST
+ * ================================================================================ Copyright (C)
+ * 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================ Licensed under
+ * the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License. ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.xacml.rest.handler;
+
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+
+public class DictionaryHandlerTest {
+  @Test
+  public void negTestHandler() {
+    // Set the system property temporarily
+    String systemKey = "dictionary.impl.className";
+    String oldProperty = System.getProperty(systemKey);
+    System.setProperty(systemKey, "foobar");
+
+    // Run negative test on instance
+    assertNull(DictionaryHandler.getInstance());
+
+    // Restore the original system property
+    if (oldProperty != null) {
+      System.setProperty(systemKey, oldProperty);
+    } else {
+      System.clearProperty(systemKey);
+    }
+  }
+}
