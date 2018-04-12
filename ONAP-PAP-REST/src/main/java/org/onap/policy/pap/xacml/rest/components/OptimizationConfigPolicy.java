882850a0f5a147c45749facf2e78bccafce1760d
Michael Mokry <mm117s@att.com>
2018-04-12 09:45:06 -0500

    OOF Policy Config File Creation Issue
    Fixed the issue with creating the config file with the wrong file name
    so that the PDP can pick it up when pushing the policy and getCOnfig
    will return the Config retrieved rather than a CONFIG_NOT_FOUND 400
    status.
    
    I found this issue in the new CSIT tests for HPA policies and this
    should also resolve the failed test in Integration as well.
    
    Change-Id: I617133a188f3ba915feafd79da4f3e9d0b0eb4a6
    Issue-ID: POLICY-738
    Signed-off-by: Michael Mokry <mm117s@att.com>

--- ONAP-PAP-REST/src/main/java/org/onap/policy/pap/xacml/rest/components/OptimizationConfigPolicy.java
+++ ONAP-PAP-REST/src/main/java/org/onap/policy/pap/xacml/rest/components/OptimizationConfigPolicy.java
@@ -88,10 +88,12 @@ public class OptimizationConfigPolicy extends Policy {
 
 	//save configuration of the policy based on the policyname
 	private void saveConfigurations(String policyName, String jsonBody) {
+		
+		if(policyName.endsWith(".xml")){
+			policyName = policyName.replace(".xml", "");
+		}
+		
 		try (PrintWriter out = new PrintWriter(CONFIG_HOME + File.separator + policyName +".json");){
-			if(policyName.endsWith(".xml")){
-				policyName = policyName.replace(".xml", "");
-			}
 			out.println(jsonBody);
 		} catch (Exception e) {
 			LOGGER.error("Exception Occured While writing Configuration data"+e);
