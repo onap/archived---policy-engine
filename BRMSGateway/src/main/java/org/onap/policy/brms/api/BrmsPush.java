1da88553b5bc956d0d4bf7d9f460e81e25a5fe88
liamfallon <liam.fallon@ericsson.com>
2018-05-16 13:59:46 +0100

    Upgrade SNAPSHOT to 1.2.2
    These reviews must be merged in order because of layering.
    
    This review should be merged SECOND
    
    Note: In order to get past the following layering problems, there
    are temporary chages to the following POMs to temporarily use 1.2.1-SNAPSHOT
    for those dependencies. They are
    ONAP-PAP-REST/pom.xml
     org.onap.policy.drools-applications.controlloop.common.policy-yaml
    ONAP-PDP/pom.xml
     org.onap.policy.drools-pdp.policy-endpoints
    
    A separate review will be used to restore these dependencies to 1.2.2-SNAPSHOT
    once the drools-pdp and drools-applications reviews are submitted and merged
    
    PLD added version.properties
    
    Issue-ID: POLICY-798
    Change-Id: Id7e83f2ba12181c63cc8845b7eeccf2004d2bab9
    Signed-off-by: liamfallon <liam.fallon@ericsson.com>
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>
    Signed-off-by: liamfallon <liam.fallon@ericsson.com>

--- BRMSGateway/src/main/java/org/onap/policy/brms/api/BrmsPush.java
+++ BRMSGateway/src/main/java/org/onap/policy/brms/api/BrmsPush.java
@@ -118,7 +118,7 @@ public class BrmsPush {
     private static final Logger LOGGER = FlexLogger.getLogger(BrmsPush.class.getName());
     private static final String PROJECTSLOCATION = "RuleProjects";
     private static final String[] GOALS = {"clean", "deploy"};
-    private static final String DEFAULT_VERSION = "1.2.1-SNAPSHOT";
+    private static final String DEFAULT_VERSION = "1.2.2";
     private static final String DEPENDENCY_FILE = "dependency.json";
     private static final String BRMSPERSISTENCE = "brmsEclipselink.persistencexml";
 
