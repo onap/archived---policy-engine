95524c8ef8be0d41de8bb2b918f320e464ebb897
rb7147 <rb7147@att.com>
2018-07-18 12:50:06 -0400

    Decision BlackList Guard Enhancements
    While creating a decision Bl Guard Policy we are allowing to add
    Blacklist entries through file upload for bulk from GUI.
    
    Issue-ID: POLICY-901
    Change-Id: I4031fd4a96937b9facc330cecf72777d701d4678
    Signed-off-by: rb7147 <rb7147@att.com>

---
+++ ONAP-REST/src/test/java/org/onap/policy/rest/adapter/ReturnBlackListTest.java
@@ -0,0 +1,41 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Engine
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.rest.adapter;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ReturnBlackListTest {
+
+    @Test
+    public void testReturnBlackList() {
+        ReturnBlackList list = new ReturnBlackList();
+        list.setActionValue(1);
+        list.setEntryCheck(true);
+        list.setEntryValue("Test");
+
+        assertTrue(1 == list.getActionValue());
+        assertTrue(list.isEntryCheck());
+        assertTrue("Test".equals(list.getEntryValue()));
+    }
+
+}
