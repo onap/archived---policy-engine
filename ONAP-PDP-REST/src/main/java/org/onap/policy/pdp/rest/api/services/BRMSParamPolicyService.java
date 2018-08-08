99004d48be1bdcaceebc1cb24a01fc9efd6fb4dc
krishnajinka <kris.jinka@samsung.com>
2018-08-09 07:35:57 +0900

    Add PAP Policy parameter builder xacml policy
    Fix Sonar flagged major issue about 14 params instead of 9
    in the constructor of std pap policy class. Commit4 for
    checkstyle changes.line width
    
    Issue-ID: POLICY-1016
    Change-Id: I79c8482b7fc4e203cb18402ba2b5a68a120c57b1
    Signed-off-by: krisjinka <kris.jinka@samsung.com>

--- ONAP-PDP-REST/src/main/java/org/onap/policy/pdp/rest/api/services/BRMSParamPolicyService.java
+++ ONAP-PDP-REST/src/main/java/org/onap/policy/pdp/rest/api/services/BRMSParamPolicyService.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,66 +31,68 @@ import org.onap.policy.xacml.api.XACMLErrorConstants;
 import org.onap.policy.xacml.std.pap.StdPAPPolicy;
 
 /**
- * BRMS Param Policy Implementation.  
- * 
+ * BRMS Param Policy Implementation.
+ *
  * @version 0.1
  */
-public class BRMSParamPolicyService{
-	private static final Logger LOGGER = FlexLogger.getLogger(BRMSParamPolicyService.class.getName());
-	private PAPServices papServices = null;
-	
-	private PolicyParameters policyParameters = null;
-	private String message = null;
-	private String policyName = null;
-	private String policyScope = null;
-	private String date = null; 
-	private Map<AttributeType, Map<String, String>> drlRuleAndUIParams = null;
-	
-	public BRMSParamPolicyService(String policyName, String policyScope,
-			PolicyParameters policyParameters, String date) {
-		this.policyParameters = policyParameters;
-		this.policyName = policyName;
-		this.policyScope = policyScope;
-		this.date = date;
-		papServices = new PAPServices();
-	}
+public class BRMSParamPolicyService {
+    private static final Logger LOGGER = FlexLogger.getLogger(BRMSParamPolicyService.class.getName());
+    private PAPServices papServices = null;
 
-	public Boolean getValidation() {
-		boolean levelCheck = PolicyApiUtils.isNumeric(policyParameters.getRiskLevel());
-		if(!levelCheck){
-			message = XACMLErrorConstants.ERROR_DATA_ISSUE + "Incorrect Risk Level given.";
-			return false;
-		}
-		drlRuleAndUIParams = policyParameters.getAttributes();
-		if(drlRuleAndUIParams==null || drlRuleAndUIParams.isEmpty()){
-			message = XACMLErrorConstants.ERROR_DATA_ISSUE + "No Rule Attributes given.";
-			return false;
-		}
-		return true;
-	}
+    private PolicyParameters policyParameters = null;
+    private String message = null;
+    private String policyName = null;
+    private String policyScope = null;
+    private String date = null;
+    private Map<AttributeType, Map<String, String>> drlRuleAndUIParams = null;
 
-	public String getMessage() {
-		return message;
-	}
+    public BRMSParamPolicyService(String policyName, String policyScope,
+                                  PolicyParameters policyParameters, String date) {
+        this.policyParameters = policyParameters;
+        this.policyName = policyName;
+        this.policyScope = policyScope;
+        this.date = date;
+        papServices = new PAPServices();
+    }
 
-	public String getResult(boolean updateFlag) throws PolicyException {
-		String response = null;
-		String operation = null;
-		if (updateFlag){
-			operation = "update";
-		} else {
-			operation = "create";
-		}
-		// Create Policy
-		StdPAPPolicy newPAPPolicy = new StdPAPPolicy("BRMS_Param",policyName, policyParameters.getPolicyDescription(),
-				"BRMS_PARAM_RULE",updateFlag,policyScope, 
-				drlRuleAndUIParams.get(AttributeType.MATCHING), 0, "DROOLS", 
-				null, drlRuleAndUIParams.get(AttributeType.RULE), policyParameters.getRiskLevel(),
-				policyParameters.getRiskType(), String.valueOf(policyParameters.getGuard()), date, policyParameters.getControllerName(), policyParameters.getDependencyNames());
-		// Send JSON to PAP 
-		response = (String) papServices.callPAP(newPAPPolicy, new String[] {"operation="+operation, "apiflag=api", "policyType=Config"}, policyParameters.getRequestID(), "ConfigBrmsParam");
-		LOGGER.info(response);
-		return response;
-	}
+    public Boolean getValidation() {
+        boolean levelCheck = PolicyApiUtils.isNumeric(policyParameters.getRiskLevel());
+        if (!levelCheck) {
+            message = XACMLErrorConstants.ERROR_DATA_ISSUE + "Incorrect Risk Level given.";
+            return false;
+        }
+        drlRuleAndUIParams = policyParameters.getAttributes();
+        if (drlRuleAndUIParams == null || drlRuleAndUIParams.isEmpty()) {
+            message = XACMLErrorConstants.ERROR_DATA_ISSUE + "No Rule Attributes given.";
+            return false;
+        }
+        return true;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getResult(boolean updateFlag) throws PolicyException {
+        String response = null;
+        String operation = null;
+        if (updateFlag) {
+            operation = "update";
+        } else {
+            operation = "create";
+        }
+        // Create Policy
+        StdPAPPolicy newPAPPolicy = new StdPAPPolicy("BRMS_Param", policyName, policyParameters.getPolicyDescription(),
+                "BRMS_PARAM_RULE", updateFlag, policyScope,
+                drlRuleAndUIParams.get(AttributeType.MATCHING), 0, "DROOLS",
+                null, drlRuleAndUIParams.get(AttributeType.RULE), policyParameters.getRiskLevel(),
+                policyParameters.getRiskType(), String.valueOf(policyParameters.getGuard()), date,
+                policyParameters.getControllerName(), policyParameters.getDependencyNames());
+        // Send JSON to PAP
+        response = (String) papServices.callPAP(newPAPPolicy, new String[]{"operation=" + operation, "apiflag=api",
+                "policyType=Config"}, policyParameters.getRequestID(), "ConfigBrmsParam");
+        LOGGER.info(response);
+        return response;
+    }
 
 }
