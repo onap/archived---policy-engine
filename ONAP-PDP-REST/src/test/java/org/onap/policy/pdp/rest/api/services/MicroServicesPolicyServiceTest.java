6161086fbb51e4cdebfd412a9dfa473c41e96f0a
kaihlavi <l.kaihlavirt@partner.samsung.com>
2019-02-14 11:03:20 +0200

    Reformat ONAP-PDP-REST test cases
    Reformat test cases to follow ONAP style
    https://wiki.onap.org/display/DW/Java+code+style
    
    Format changes mainly consist of conversions of tabs to 4 spaces
    
    Modifications copyrights for Samsung added and a number of
    subproject names corrected for licenses
    
    Change-Id: I52106aa0ab249dc6bf3b2a9c455e53f42d0e6136
    Issue-ID: POLICY-1506
    Signed-off-by: kaihlavi <l.kaihlavirt@partner.samsung.com>

--- ONAP-PDP-REST/src/test/java/org/onap/policy/pdp/rest/api/services/MicroServicesPolicyServiceTest.java
+++ ONAP-PDP-REST/src/test/java/org/onap/policy/pdp/rest/api/services/MicroServicesPolicyServiceTest.java
@@ -1,15 +1,17 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP-PAP-REST
+ * ONAP-PDP-REST
  * ================================================================================
  * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright (C) 2019 Samsung
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +22,6 @@
 package org.onap.policy.pdp.rest.api.services;
 
 import static org.junit.Assert.*;
-
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -28,7 +29,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -40,59 +40,58 @@ public class MicroServicesPolicyServiceTest {
 
     MicroServicesPolicyService service = null;
 
-	@Before
-	public void setUp() throws Exception {
-		Properties prop = new Properties();
-		prop.load(new FileInputStream("src/test/resources/pass.xacml.pdp.properties"));
-		String succeeded = prop.getProperty("xacml.rest.pap.url");
-		List<String> paps = Arrays.asList(succeeded.split(","));
-		PAPServices.setPaps(paps);
-		PAPServices.setJunit(true);
-		
-		PolicyParameters policyParameters = new PolicyParameters();
+    @Before
+    public void setUp() throws Exception {
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("src/test/resources/pass.xacml.pdp.properties"));
+        String succeeded = prop.getProperty("xacml.rest.pap.url");
+        List<String> paps = Arrays.asList(succeeded.split(","));
+        PAPServices.setPaps(paps);
+        PAPServices.setJunit(true);
+
+        PolicyParameters policyParameters = new PolicyParameters();
         policyParameters.setPolicyConfigType(PolicyConfigType.MicroService);
         policyParameters.setPolicyName("Test.testMSPolicy");
-		policyParameters.setOnapName("DCAE");
+        policyParameters.setOnapName("DCAE");
         policyParameters.setRequestID(UUID.randomUUID());
-      	SimpleDateFormat dateformat3 = new SimpleDateFormat("dd/MM/yyyy");
-		Date date = dateformat3.parse("15/10/2016");
-		policyParameters.setTtlDate(date);
-		policyParameters.setGuard(true);
-		policyParameters.setRiskLevel("5");
-		policyParameters.setRiskType("TEST");
-		policyParameters.setConfigBody("{\"configName\":\"test\"}");
-		String policyName = "testMSPolicy";
-		String policyScope = "Test";
-		service = new MicroServicesPolicyService(policyName, policyScope, policyParameters, date.toString());
-	}
+        SimpleDateFormat dateformat3 = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = dateformat3.parse("15/10/2016");
+        policyParameters.setTtlDate(date);
+        policyParameters.setGuard(true);
+        policyParameters.setRiskLevel("5");
+        policyParameters.setRiskType("TEST");
+        policyParameters.setConfigBody("{\"configName\":\"test\"}");
+        String policyName = "testMSPolicy";
+        String policyScope = "Test";
+        service = new MicroServicesPolicyService(policyName, policyScope, policyParameters, date.toString());
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		PAPServices.setPaps(null);
-		PAPServices.setJunit(false);
-	}
+    @After
+    public void tearDown() throws Exception {
+        PAPServices.setPaps(null);
+        PAPServices.setJunit(false);
+    }
 
-	@Test
-	public final void testFirewallPolicyService() {
-		assertNotNull(service);
-	}
+    @Test
+    public final void testFirewallPolicyService() {
+        assertNotNull(service);
+    }
 
-	@Test
-	public final void testGetValidation() {
-		assertTrue(service.getValidation());
-	}
+    @Test
+    public final void testGetValidation() {
+        assertTrue(service.getValidation());
+    }
 
-	@Test
-	public final void testGetMessage() {
-		String message = service.getMessage();
-		assertNull(message);
-	}
+    @Test
+    public final void testGetMessage() {
+        String message = service.getMessage();
+        assertNull(message);
+    }
 
-	@Test
-	public final void testGetResult() throws PolicyException {
-		service.getValidation();
-		String result = service.getResult(false);
-		assertEquals("success",result);
-	}
-
+    @Test
+    public final void testGetResult() throws PolicyException {
+        service.getValidation();
+        String result = service.getResult(false);
+        assertEquals("success", result);
+    }
 }
--- ONAP-PDP-REST/src/test/java/org/onap/policy/pdp/rest/api/test/GetConfigServiceTest.java
+++ ONAP-PDP-REST/src/test/java/org/onap/policy/pdp/rest/api/test/GetConfigServiceTest.java
@@ -4,12 +4,14 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2019 Samsung
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +22,6 @@
 package org.onap.policy.pdp.rest.api.test;
 
 import static org.junit.Assert.assertEquals;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collection;
@@ -28,7 +29,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.Test;
 import org.onap.policy.api.ConfigRequestParameters;
 import org.onap.policy.api.PolicyConfigStatus;
@@ -36,35 +36,38 @@ import org.onap.policy.pdp.rest.api.models.PolicyConfig;
 import org.onap.policy.pdp.rest.api.services.GetConfigService;
 
 public class GetConfigServiceTest {
-	private static final String TEST = "test";
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void filterMethodTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
-		ConfigRequestParameters configRequestParameters = new ConfigRequestParameters();
-		GetConfigService getConfigService= new GetConfigService(configRequestParameters, null);
-		Method filter = GetConfigService.class.getDeclaredMethod("filterResults", Collection.class,ConfigRequestParameters.class);
-		filter.setAccessible(true);
-		List<PolicyConfig> policyConfigs = new LinkedList<>();
-		
-		List<PolicyConfig> filterResults = (List<PolicyConfig>) filter.invoke(getConfigService, policyConfigs,configRequestParameters);
-		assertEquals(PolicyConfigStatus.CONFIG_NOT_FOUND, filterResults.get(0).getPolicyConfigStatus());
-		// Check again with some values 
-		configRequestParameters.setPolicyName(TEST);
-		configRequestParameters.setOnapName(TEST);
-		configRequestParameters.setConfigName(TEST);
-		Map<String,String> configAttributes = new HashMap<>();
-		configAttributes.put(TEST, TEST);
-		configRequestParameters.setConfigAttributes(configAttributes);
-		PolicyConfig pConfig = new PolicyConfig();
-		pConfig.setPolicyName(TEST);
-		Map<String,String> matching = new HashMap<>();
-		matching.put("ONAPName", TEST);
-		matching.put("ConfigName", TEST);
-		matching.put("TEST", TEST);
-		pConfig.setMatchingConditions(matching);
-		policyConfigs.add(pConfig);
-		filterResults = (List<PolicyConfig>) filter.invoke(getConfigService, policyConfigs,configRequestParameters);
-		assertEquals(PolicyConfigStatus.CONFIG_NOT_FOUND, filterResults.get(0).getPolicyConfigStatus());
-	}
+    private static final String TEST = "test";
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void filterMethodTest() throws NoSuchMethodException, SecurityException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException {
+        ConfigRequestParameters configRequestParameters = new ConfigRequestParameters();
+        GetConfigService getConfigService = new GetConfigService(configRequestParameters, null);
+        Method filter = GetConfigService.class.getDeclaredMethod("filterResults", Collection.class,
+                ConfigRequestParameters.class);
+        filter.setAccessible(true);
+        List<PolicyConfig> policyConfigs = new LinkedList<>();
+
+        List<PolicyConfig> filterResults =
+                (List<PolicyConfig>) filter.invoke(getConfigService, policyConfigs, configRequestParameters);
+        assertEquals(PolicyConfigStatus.CONFIG_NOT_FOUND, filterResults.get(0).getPolicyConfigStatus());
+        // Check again with some values
+        configRequestParameters.setPolicyName(TEST);
+        configRequestParameters.setOnapName(TEST);
+        configRequestParameters.setConfigName(TEST);
+        Map<String, String> configAttributes = new HashMap<>();
+        configAttributes.put(TEST, TEST);
+        configRequestParameters.setConfigAttributes(configAttributes);
+        PolicyConfig pConfig = new PolicyConfig();
+        pConfig.setPolicyName(TEST);
+        Map<String, String> matching = new HashMap<>();
+        matching.put("ONAPName", TEST);
+        matching.put("ConfigName", TEST);
+        matching.put("TEST", TEST);
+        pConfig.setMatchingConditions(matching);
+        policyConfigs.add(pConfig);
+        filterResults = (List<PolicyConfig>) filter.invoke(getConfigService, policyConfigs, configRequestParameters);
+        assertEquals(PolicyConfigStatus.CONFIG_NOT_FOUND, filterResults.get(0).getPolicyConfigStatus());
+    }
 }
