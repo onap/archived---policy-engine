17125f50410afa6ea83b0cb430bc19324c5fed03
Rashmi Pujar <rashmi.pujar@bell.ca>
2019-03-28 11:53:51 -0400

    FIx Sonar Issues reported on ActionPolicyController
    - code cleanup, reduce cognitive complexity, extract constants etc
    
    Issue-ID: POLICY-1615
    
    Change-Id: Ie0f634b1848076bba4c3ea3cbf9252b9faba0f8f
    Signed-off-by: Rashmi Pujar <rashmi.pujar@bell.ca>

--- POLICY-SDK-APP/src/main/java/org/onap/policy/admin/PolicyAdapter.java
+++ POLICY-SDK-APP/src/main/java/org/onap/policy/admin/PolicyAdapter.java
@@ -52,7 +52,7 @@ public class PolicyAdapter {
         }
 
         if("Action".equalsIgnoreCase(policyAdapter.getPolicyType())){
-            new ActionPolicyController().prePopulateActionPolicyData(policyAdapter, entity);
+            new ActionPolicyController().prePopulateActionPolicyData(policyAdapter);
         }
         if("Decision".equalsIgnoreCase(policyAdapter.getPolicyType())){
             new DecisionPolicyController().prePopulateDecisionPolicyData(policyAdapter, entity);
