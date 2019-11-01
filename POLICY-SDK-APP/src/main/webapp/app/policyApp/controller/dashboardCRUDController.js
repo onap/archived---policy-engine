4c66a988008e2cf147b05b5e99732123547baec4
uj426b <uj426b@att.com>
2019-11-01 14:42:28 -0400

    Fixed Sonar issues for Console Logs
    Issue-ID: POLICY-2153
    Change-Id: Id21a9dd4910ff0c831e49a1539f130ade1779577
    Signed-off-by: utkarshJ <uj426b@att.com>

--- POLICY-SDK-APP/src/main/webapp/app/policyApp/controller/dashboardCRUDController.js
+++ POLICY-SDK-APP/src/main/webapp/app/policyApp/controller/dashboardCRUDController.js
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP Policy Engine
  * ================================================================================
- * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018, 2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,20 +19,18 @@
  */
 
 app.controller('policyDashboardCRUDDataController', function ($scope, PolicyAppService, modalService, $modal){
-	console.log("policyDashboardCRUDDataController called");
-	
-	$('#dashBoardAdvanceSearch').hide();
-	
+    
+    $('#dashBoardAdvanceSearch').hide();
+    
     $scope.papCRUDTableDatasTemp = [];
     
     $scope.dashboardAdsearch = { isDelected: 'both', stage: 'both',  scope: "", ttlDate_after: "", ttlDate_before: ""};
 
     PolicyAppService.getData('get_DashboardPolicyCRUDData').then(function(data){
 
-		var j = data;
-		$scope.data = JSON.parse(j.data);
-		console.log($scope.data);
-		$scope.papStatusCRUDDatas =JSON.parse($scope.data.papStatusCRUDData);
+        var j = data;
+        $scope.data = JSON.parse(j.data);
+        $scope.papStatusCRUDDatas =JSON.parse($scope.data.papStatusCRUDData);
         if($scope.papStatusCRUDDatas != null){
             for(i = 0; i < $scope.papStatusCRUDDatas.length; i++){
                 $scope.papCRUDTableDatasTemp.push($scope.papStatusCRUDDatas[i].map);
@@ -40,117 +38,106 @@ app.controller('policyDashboardCRUDDataController', function ($scope, PolicyAppS
             
             $scope.papCRUDTableDatas = $scope.papCRUDTableDatasTemp;
         } 
-		
-	},function(error){
-		console.log("failed");
-	});
+        
+    });
 
-	$scope.papCRUDStatusDatas = {
-		data : 'papCRUDTableDatas',
-		enableFiltering: true,
-		columnDefs: [{ field: 'id', displayName :'id'},
-		 		 	{field: 'scope', displayName :'Scope'},
-					{field: 'policyName', displayName :'Policy Name'},
-					{field: 'version', displayName :'Version'},
-					{field: 'stage', displayName :'Stage'},
-					{field: 'createdBy', displayName :'Created By'},
-					{field: 'deleted', displayName :'Deleted'},
-					{field: 'deleteReasonCode', displayName :'Deleted Reason'},
-					{field: 'deletedBy' , displayName :'Deleted By'},
-					{field: 'modifiedBy' , displayName :'Modified By'},
-					{field: 'createdDate',  displayName :'Created Date'},
-					{field: 'modifiedDate',  displayName :'Modified Date'}
-		],
+    $scope.papCRUDStatusDatas = {
+        data : 'papCRUDTableDatas',
+        enableFiltering: true,
+        columnDefs: [{ field: 'id', displayName :'id'},
+                      {field: 'scope', displayName :'Scope'},
+                    {field: 'policyName', displayName :'Policy Name'},
+                    {field: 'version', displayName :'Version'},
+                    {field: 'stage', displayName :'Stage'},
+                    {field: 'createdBy', displayName :'Created By'},
+                    {field: 'deleted', displayName :'Deleted'},
+                    {field: 'deleteReasonCode', displayName :'Deleted Reason'},
+                    {field: 'deletedBy' , displayName :'Deleted By'},
+                    {field: 'modifiedBy' , displayName :'Modified By'},
+                    {field: 'createdDate',  displayName :'Created Date'},
+                    {field: 'modifiedDate',  displayName :'Modified Date'}
+        ],
         onRegisterApi: function(gridApi){
-        	$scope.gridApi = gridApi;
+            $scope.gridApi = gridApi;
         }
-	};
-	
+    };
+    
     $('#ttlDate_after').datepicker({
-    	dateFormat: 'yy-mm-dd',
-    	changeMonth: true,
-    	changeYear: true,
-    	onSelect: function(date) {
-    		angular.element($('#ttlDate_after')).triggerHandler('input');
-    	}
+        dateFormat: 'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        onSelect: function(date) {
+            angular.element($('#ttlDate_after')).triggerHandler('input');
+        }
     });
     
     $('#ttlDate_before').datepicker({
-    	dateFormat: 'yy-mm-dd',
-    	changeMonth: true,
-    	changeYear: true,
-    	onSelect: function(date) {
-    		angular.element($('#ttlDate_before')).triggerHandler('input');
-    	}
+        dateFormat: 'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        onSelect: function(date) {
+            angular.element($('#ttlDate_before')).triggerHandler('input');
+        }
     });
     
     $scope.refresh = function(){
-    	$scope.modal('advancedSearch', true);
-    	$scope.temp.policy = "";
+        $scope.modal('advancedSearch', true);
+        $scope.temp.policy = "";
     };
     
     
-	$scope.advancedSearch = function(){
+    $scope.advancedSearch = function(){
 
-		 $('#dashBoardAdvanceSearch').toggle();
-		 if($('#advancedSearchArrow').hasClass('arrowdown')){
-			 $('#advancedSearchArrow').removeClass("arrowdown");
-			 $('#advancedSearchArrow').addClass("arrowup"); 
-			 
-		 }else{
-			 $('#advancedSearchArrow').removeClass("arrowup");
-			 $('#advancedSearchArrow').addClass("arrowdown"); 
-		 }
-	}
-	
+         $('#dashBoardAdvanceSearch').toggle();
+         if($('#advancedSearchArrow').hasClass('arrowdown')){
+             $('#advancedSearchArrow').removeClass("arrowdown");
+             $('#advancedSearchArrow').addClass("arrowup"); 
+             
+         }else{
+             $('#advancedSearchArrow').removeClass("arrowup");
+             $('#advancedSearchArrow').addClass("arrowdown"); 
+         }
+    }
+    
    
     $scope.startAdvancedSearch = function(data){
-    	
-		 console.log("startAdvancedSearch called");
-		 console.log(data.isDelected);
-		 console.log(data.stage);
-		 console.log(data.scope);
-		 console.log(data.ttlDate_after);
-		 console.log(data.ttlDate_before);
-		 
-		 if(data.scope == null){
-			 return;
-		 }
-		 
+        
+         
+         if(data.scope == null){
+             return;
+         }
+         
        var uuu = "dashboardController/dashboardAdvancedSearch.htm";
         
         var postData={policyData: data};
- 		$.ajax({
- 			type : 'POST',
- 			url : uuu,
- 			dataType: 'json',
- 			contentType: 'application/json',
- 			data: JSON.stringify(postData),
- 			success : function(data){
- 				 console.log("dashboardAdvancedSearch data returned: " + data);
- 				
+         $.ajax({
+             type : 'POST',
+             url : uuu,
+             dataType: 'json',
+             contentType: 'application/json',
+             data: JSON.stringify(postData),
+             success : function(data){
+                 
                  $scope.$apply(function(){  
-                	
-     				var j = data;
-     				$scope.data = JSON.parse(j.data);
-     				console.log($scope.data);
-     				$scope.papStatusCRUDDatas =JSON.parse($scope.data.policyStatusCRUDData);
-     				
-     				$scope.papCRUDTableDatasTemp = [];
-     		       
-     	            for(i = 0; i < $scope.papStatusCRUDDatas.length; i++){
-     	                $scope.papCRUDTableDatasTemp.push($scope.papStatusCRUDDatas[i].map);
-     	            }
-     	            
-     	            $scope.papCRUDTableDatas = $scope.papCRUDTableDatasTemp;
-     	            
-     	           $scope.gridApi.grid.refresh();
+                    
+                     var j = data;
+                     $scope.data = JSON.parse(j.data);
+                     $scope.papStatusCRUDDatas =JSON.parse($scope.data.policyStatusCRUDData);
+                     
+                     $scope.papCRUDTableDatasTemp = [];
+                    
+                     for(i = 0; i < $scope.papStatusCRUDDatas.length; i++){
+                         $scope.papCRUDTableDatasTemp.push($scope.papStatusCRUDDatas[i].map);
+                     }
+                     
+                     $scope.papCRUDTableDatas = $scope.papCRUDTableDatasTemp;
+                     
+                    $scope.gridApi.grid.refresh();
                });
- 			},
- 			error : function(data){
- 				console.log("dashboardAdvancedSearch Failed: data returned as " + data);
- 			}
- 		});
+             },
+             error : function(data){
+             }
+         });
     };
 
 });
--- POLICY-SDK-APP/src/main/webapp/app/policyApp/policy-models/Editor/js/controllers/policySearchManager.js
+++ POLICY-SDK-APP/src/main/webapp/app/policyApp/policy-models/Editor/js/controllers/policySearchManager.js
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP Policy Engine
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017, 2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,194 +18,176 @@
  * ============LICENSE_END=========================================================
  */
 app.controller('PolicySearchController', [
-        '$scope', '$q', '$window', '$cookies', 'policyManagerConfig', 'item', 'policyNavigator', 'policyUploader', 'Notification','PolicyAppService',
-        function($scope, $q, $Window, $cookies, policyManagerConfig, Item, PolicyNavigator, PolicyUploader, Notification, PolicyAppService ) {
-        	
-        $scope.isDisabled = true;
-        $scope.superAdminId = false;
-        $scope.exportPolicyId = false;
-        $scope.importPolicyId = false;
-        $scope.createScopeId = false;
-        $scope.deleteScopeId = false;
-        $scope.renameId = false;
-        $scope.createPolicyId = false;
-        $scope.cloneId = false;
-        $scope.editPolicyId = false;
-        $scope.switchVersionId = false;
-        $scope.describePolicyId = false;
-        $scope.viewPolicyId = false;
-        $scope.deletePolicyId = false;
-        PolicyAppService.getData('get_LockDownData').then(function(data){
-        	var j = data;
-        	$scope.data = JSON.parse(j.data);
-        	$scope.lockdowndata = JSON.parse($scope.data.lockdowndata);
-        	if($scope.lockdowndata[0].lockdown == true){
-        		$scope.isDisabled = true;
-        	}else{
-        		$scope.isDisabled = false;
-        	}
-        	console.log($scope.data);
-        },function(error){
-        	console.log("failed");
-        });
+    '$scope', '$q', '$window', '$cookies', 'policyManagerConfig', 'item', 'policyNavigator', 'policyUploader', 'Notification','PolicyAppService',
+    function($scope, $q, $Window, $cookies, policyManagerConfig, Item, PolicyNavigator, PolicyUploader, Notification, PolicyAppService ) {
         
-        PolicyAppService.getData('getDictionary/get_DescriptiveScopeByName').then(function(data){
-        	var j = data;
-        	$scope.data = JSON.parse(j.data);
-        	console.log($scope.data);
-        	$scope.descriptiveScopeDictionaryDatas = JSON.parse($scope.data.descriptiveScopeDictionaryDatas);	
-        }, function (error) {
-        	console.log("failed");
-        });
+    $scope.isDisabled = true;
+    $scope.superAdminId = false;
+    $scope.exportPolicyId = false;
+    $scope.importPolicyId = false;
+    $scope.createScopeId = false;
+    $scope.deleteScopeId = false;
+    $scope.renameId = false;
+    $scope.createPolicyId = false;
+    $scope.cloneId = false;
+    $scope.editPolicyId = false;
+    $scope.switchVersionId = false;
+    $scope.describePolicyId = false;
+    $scope.viewPolicyId = false;
+    $scope.deletePolicyId = false;
+    PolicyAppService.getData('get_LockDownData').then(function(data){
+        var j = data;
+        $scope.data = JSON.parse(j.data);
+        $scope.lockdowndata = JSON.parse($scope.data.lockdowndata);
+        if($scope.lockdowndata[0].lockdown == true){
+            $scope.isDisabled = true;
+        }else{
+            $scope.isDisabled = false;
+        }
+    },function(error){
+    });
+    
+    PolicyAppService.getData('getDictionary/get_DescriptiveScopeByName').then(function(data){
+        var j = data;
+        $scope.data = JSON.parse(j.data);
+        $scope.descriptiveScopeDictionaryDatas = JSON.parse($scope.data.descriptiveScopeDictionaryDatas);    
+    });
 
-        PolicyAppService.getData('getDictionary/get_OnapNameDataByName').then(function(data){
-        	var j = data;
-        	$scope.data = JSON.parse(j.data);
-        	console.log($scope.data);
-        	$scope.onapNameDictionaryDatas = JSON.parse($scope.data.onapNameDictionaryDatas);	
-        }, function (error) {
-        	console.log("failed");
-        });
+    PolicyAppService.getData('getDictionary/get_OnapNameDataByName').then(function(data){
+        var j = data;
+        $scope.data = JSON.parse(j.data);
+        $scope.onapNameDictionaryDatas = JSON.parse($scope.data.onapNameDictionaryDatas);    
+    });
 
-        PolicyAppService.getData('getDictionary/get_VSCLActionDataByName').then(function(data){
-        	var j = data;
-        	$scope.data = JSON.parse(j.data);
-        	console.log($scope.data);
-        	$scope.vsclActionDictionaryDatas = JSON.parse($scope.data.vsclActionDictionaryDatas);	
-        }, function (error) {
-        	console.log("failed");
-        });
+    PolicyAppService.getData('getDictionary/get_VSCLActionDataByName').then(function(data){
+        var j = data;
+        $scope.data = JSON.parse(j.data);
+        $scope.vsclActionDictionaryDatas = JSON.parse($scope.data.vsclActionDictionaryDatas);    
+    });
 
-        PolicyAppService.getData('getDictionary/get_VNFTypeDataByName').then(function(data){
-        	var j = data;
-        	$scope.data = JSON.parse(j.data);
-        	console.log($scope.data);
-        	$scope.vnfTypeDictionaryDatas = JSON.parse($scope.data.vnfTypeDictionaryDatas);	
-        }, function (error) {
-        	console.log("failed");
-        });
+    PolicyAppService.getData('getDictionary/get_VNFTypeDataByName').then(function(data){
+        var j = data;
+        $scope.data = JSON.parse(j.data);
+        $scope.vnfTypeDictionaryDatas = JSON.parse($scope.data.vnfTypeDictionaryDatas);    
+    });
 
-        
-        PolicyAppService.getData('get_UserRolesData').then(function (data) {
-        	var j = data;
-        	$scope.data = JSON.parse(j.data);
-        	console.log($scope.data);
-        	$scope.userRolesDatas = JSON.parse($scope.data.userRolesDatas);
-        	console.log($scope.userRolesDatas);
-        	if($scope.userRolesDatas[0] == 'super-admin'){
-        		$scope.superAdminId = true;
-        		$scope.createPolicyId = true; 
-        		$scope.editPolicyId = true;
-        		$scope.describePolicyId = true;
-        		$scope.viewPolicyId = true;
-        	}else if($scope.userRolesDatas[0] == 'super-editor' || $scope.userRolesDatas[0] == 'editor' || $scope.userRolesDatas[0] == 'admin'){
-        		$scope.editPolicyId = true;
-        		$scope.createPolicyId = true;
-        		$scope.describePolicyId = true;
-        		$scope.viewPolicyId = true;
-        	}else if($scope.userRolesDatas[0] == 'super-guest' || $scope.userRolesDatas[0] == 'guest'){
-        		$scope.describePolicyId = true;
-        		$scope.viewPolicyId = true;
-        	}
-        }, function (error) {
-     	      console.log("failed");
-     	});
-        
-        $scope.config = policyManagerConfig;
-        $scope.reverse = false;
-        $scope.predicate = ['model.type', 'model.name'];
-        $scope.order = function(predicate) {
-            $scope.reverse = ($scope.predicate[1] === predicate) ? !$scope.reverse : false;
-            $scope.predicate[1] = predicate;
-        };
+    
+    PolicyAppService.getData('get_UserRolesData').then(function (data) {
+        var j = data;
+        $scope.data = JSON.parse(j.data);
+        $scope.userRolesDatas = JSON.parse($scope.data.userRolesDatas);
+        if($scope.userRolesDatas[0] == 'super-admin'){
+            $scope.superAdminId = true;
+            $scope.createPolicyId = true; 
+            $scope.editPolicyId = true;
+            $scope.describePolicyId = true;
+            $scope.viewPolicyId = true;
+        }else if($scope.userRolesDatas[0] == 'super-editor' || $scope.userRolesDatas[0] == 'editor' || $scope.userRolesDatas[0] == 'admin'){
+            $scope.editPolicyId = true;
+            $scope.createPolicyId = true;
+            $scope.describePolicyId = true;
+            $scope.viewPolicyId = true;
+        }else if($scope.userRolesDatas[0] == 'super-guest' || $scope.userRolesDatas[0] == 'guest'){
+            $scope.describePolicyId = true;
+            $scope.viewPolicyId = true;
+        }
+    });
+    
+    $scope.config = policyManagerConfig;
+    $scope.reverse = false;
+    $scope.predicate = ['model.type', 'model.name'];
+    $scope.order = function(predicate) {
+    $scope.reverse = ($scope.predicate[1] === predicate) ? !$scope.reverse : false;
+    $scope.predicate[1] = predicate;
+    };
 
-        $scope.query = '';
-        $scope.temp = new Item();
-        $scope.policyNavigator = new PolicyNavigator();
+    $scope.query = '';
+    $scope.temp = new Item();
+    $scope.policyNavigator = new PolicyNavigator();
 
-        $scope.setTemplate = function(name) {
-            $scope.viewTemplate = $cookies.viewTemplate = name;
-        };
+    $scope.setTemplate = function(name) {
+    $scope.viewTemplate = $cookies.viewTemplate = name;
+    };
 
-        $scope.touch = function(item) {
-            item = item instanceof Item ? item : new Item();
-            item.revert();
-            $scope.temp = item;
-        };
+    $scope.touch = function(item) {
+    item = item instanceof Item ? item : new Item();
+    item.revert();
+    $scope.temp = item;
+    };
 
-        $scope.smartClick = function(item) {
-            if (item.isFolder()) {
-                return $scope.policyNavigator.folderClick(item);
-            }
-            if (item.isEditable()) {
-                return $scope.openEditItem(item);
-            }
-        };
+    $scope.smartClick = function(item) {
+    if (item.isFolder()) {
+    return $scope.policyNavigator.folderClick(item);
+    }
+    if (item.isEditable()) {
+    return $scope.openEditItem(item);
+    }
+    };
 
-        $scope.openEditItem = function(item) {
-            item.getContent();
-            $scope.modal('createNewPolicy');
-            return $scope.touch(item);
-        };
+    $scope.openEditItem = function(item) {
+    item.getContent();
+    $scope.modal('createNewPolicy');
+    return $scope.touch(item);
+    };
 
-        $scope.modal = function(id, hide) {
-            return $('#' + id).modal(hide ? 'hide' : 'show');
-        };
+    $scope.modal = function(id, hide) {
+    return $('#' + id).modal(hide ? 'hide' : 'show');
+    };
 
-        $scope.isInThisPath = function(path) {
-            var currentPath = $scope.policyNavigator.currentPath.join('/');
-            return currentPath.indexOf(path) !== -1;
-        };
+    $scope.isInThisPath = function(path) {
+    var currentPath = $scope.policyNavigator.currentPath.join('/');
+    return currentPath.indexOf(path) !== -1;
+    };
        
-        $scope.searchPolicy = function(searchContent){
-        	if(searchContent != undefined){
-        		var uuu = "searchPolicy";
-        		var postData = {searchdata : searchContent};
-        		$.ajax({
-        			type : 'POST',
-        			url : uuu,
-        			dataType: 'json',
-        			contentType: 'application/json',
-        			data: JSON.stringify(postData),
-        			success : function(data){
-        				$scope.$apply(function(){
-        					var searchdata = data.result;
-        					if(searchdata.length > 0){
-        						if(searchdata[0] == "Exception"){
-        							Notification.error(searchdata[1]);
-        						}else{
-        							$scope.policyNavigator.searchrefresh(searchdata);  
-        						}
-        					}else{
-        						Notification.info("No Matches Found with your Search");
-        					}
-        				});     
-        			},
-        			error : function(data){
-        				Notification.error("Error while Searching.");
-        			}
-        		});
-        	}else{
-        		Notification.error("No data has been entered or selected to search");
-        	}
-        };
-       
-       $scope.refresh = function(searchData){
-    	   $scope.policyNavigator.searchrefresh(null);
-       };
-         
-        $scope.getQueryParam = function(param) {
-            var found;
-            window.location.search.substr(1).split('&').forEach(function(item) {
-                if (param ===  item.split('=')[0]) {
-                    found = item.split('=')[1];
-                    return false;
+    $scope.searchPolicy = function(searchContent){
+        if(searchContent != undefined){
+            var uuu = "searchPolicy";
+            var postData = {searchdata : searchContent};
+            $.ajax({
+                type : 'POST',
+                url : uuu,
+                dataType: 'json',
+                contentType: 'application/json',
+                data: JSON.stringify(postData),
+                success : function(data){
+                    $scope.$apply(function(){
+                        var searchdata = data.result;
+                        if(searchdata.length > 0){
+                            if(searchdata[0] == "Exception"){
+                                Notification.error(searchdata[1]);
+                            }else{
+                                $scope.policyNavigator.searchrefresh(searchdata);  
+                            }
+                        }else{
+                            Notification.info("No Matches Found with your Search");
+                        }
+                    });     
+                },
+                error : function(data){
+                    Notification.error("Error while Searching.");
                 }
             });
-            return found;
-        };
+        }else{
+            Notification.error("No data has been entered or selected to search");
+        }
+    };
+       
+       $scope.refresh = function(searchData){
+           $scope.policyNavigator.searchrefresh(null);
+       };
+     
+    $scope.getQueryParam = function(param) {
+    var found;
+    window.location.search.substr(1).split('&').forEach(function(item) {
+    if (param ===  item.split('=')[0]) {
+    found = item.split('=')[1];
+    return false;
+    }
+    });
+    return found;
+    };
 
-        $scope.isWindows = $scope.getQueryParam('server') === 'Windows';
-        $scope.policyNavigator.searchrefresh(null);
-        $scope.policyNavigator.setSearchModalActiveStatus();
+    $scope.isWindows = $scope.getQueryParam('server') === 'Windows';
+    $scope.policyNavigator.searchrefresh(null);
+    $scope.policyNavigator.setSearchModalActiveStatus();
     }]);
